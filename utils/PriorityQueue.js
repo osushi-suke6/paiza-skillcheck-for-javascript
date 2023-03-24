4a9d64caa812359e30e841955454403984d7fd6c
Sukeroku Hanakawado <osushi.suke6@gmail.com>
2023-03-24 22:13:47 +0900

    add: implement push

--- utils/PriorityQueue.js
+++ utils/PriorityQueue.js
@@ -3,7 +3,25 @@ class PriorityQueue {
     this.heap = [];
   }
 
-  push(item, priority) {}
+  push(item, priority) {
+    const newNode = { item, priority };
+    this.heap.push(newNode);
+
+    let currentNodeIdx = this.heap.length - 1;
+    let currentNodeParentIdx = Math.ceil(currentNodeIdx / 2) - 1;
+
+    while (
+      currentNodeParentIdx >= 0 &&
+      newNode.priority > this.heap[currentNodeParentIdx].priority
+    ) {
+      const parent = this.heap[currentNodeParentIdx];
+      this.heap[currentNodeParentIdx] = newNode;
+      this.heap[currentNodeIdx] = parent;
+
+      currentNodeIdx = currentNodeParentIdx;
+      currentNodeParentIdx = Math.ceil(currentNodeIdx / 2) - 1;
+    }
+  }
 
   pop() {}
 
