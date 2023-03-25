511edeed0285c75df308b954fdf4abdefabd4704
Sukeroku Hanakawado <osushi.suke6@gmail.com>
2023-03-26 00:14:22 +0900

    add: main.ts

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+process.stdin.resume().setEncoding('utf8');
+const lines: string[] = [];
+require('readline')
+  .createInterface({ input: process.stdin })
+  .on('line', (input: string) => lines.push(input))
+  .on('close', main);
+
+function main() {
+  // write code here
+}
