8a78579bc7112fa8c0326ced0d5da9fd275d48c1
Sukeroku Hanakawado <osushi.suke6@gmail.com>
2023-03-26 21:10:22 +0900

    add: transpiled files

---
+++ dist/main.js
@@ -0,0 +1,10 @@
+"use strict";
+process.stdin.resume().setEncoding('utf8');
+const lines = [];
+require('readline')
+    .createInterface({ input: process.stdin })
+    .on('line', (input) => lines.push(input))
+    .on('close', main);
+function main() {
+    // write code here
+}
